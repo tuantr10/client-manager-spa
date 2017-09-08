eb1b70a3a4b3da9d06bb66bdce7f772f304a955f
Tuan Dung TRAN <mcafee92@gmail.com>
2017-09-08 19:19:17 +0900

    update database, update CREATE statement

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let db = new sqlite3.Database('db/employees.db', (err) => {
 	console.log('Connected to employees database')
 
 	db.serialize(() => {
-		db.run("CREATE TABLE if not exists employee_info (name VARCHAR(255), address VARCHAR(255), phone VARCHAR(255), email VARCHAR(255), salary REAL)");
+		db.run("CREATE TABLE employee_info(`name` VARCHAR ( 255 ) NOT NULL, `address` VARCHAR ( 255 ) NOT NULL, `phone` VARCHAR ( 255 ) NOT NULL, `email` VARCHAR ( 255 ) NOT NULL UNIQUE, `salary` REAL NOT NULL, `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE)");
 	});
 
 	db.close();
