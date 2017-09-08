6cfb26e76562c0146f8ce5bc5398d67b69ae16b2
Tuan Dung TRAN <mcafee92@gmail.com>
2017-09-08 18:50:33 +0900

    fix docker file, add sqlite3, initial database

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,17 @@
-FROM node:8
+FROM node:6.11.3
 
 RUN mkdir /app
 WORKDIR /app
 
 RUN apt-get update && apt-get install -y \
         sqlite3
-RUN mkdir /db
-RUN sqlite3 /db/client.db
+RUN mkdir /app/db
 
 COPY package.json /app
+RUN npm install -g node-gyp
 RUN npm install
 COPY . /app
 
-EXPOSE 8081
+EXPOSE 3000
 
 CMD ["npm", "start"]
--- apps/api/module.js
+++ apps/api/module.js
@@ -1,5 +1,19 @@
-exports.init = function(app) {
-	app.get('/api/me', function (req, res) {
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database(':memory:', (err) => {
+	if (err) {
+		return console.error(err.message)
+	}
+	console.log('Connected to the in-memory SQlite database.')
+
+	db.serialize(() => {
+		db.run("CREATE TABLE if not exists employee_info (name VARCHAR(255), address VARCHAR(255), phone VARCHAR(255), email VARCHAR(255), salary REAL)");
+	});
+
+	db.close();
+});
+
+exports.init = (app) => {
+	app.get('/api/me', (req, res) => {
 		res.json({})
 	})
 }
