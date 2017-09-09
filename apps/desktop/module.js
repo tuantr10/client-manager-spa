657af0a8462ea7eb1d75545e55a89b8046cd8fb7
Tuan Dung TRAN <mcafee92@gmail.com>
2017-09-09 18:17:24 +0900

    add frontend basic template file

--- apps/desktop/module.js
+++ apps/desktop/module.js
@@ -1,5 +1,19 @@
-exports.init = (app) => {
-	app.get('/', (req, res) => {
-		res.send('Hello world');
-	});
-};
+exports.init = function(app) {
+	var ect = require('ect')
+	var viewPath = __dirname + '/views'
+
+	app.set('view engine', 'ect')
+	app.set('views', viewPath);
+
+	app.engine('ect', ect({
+		//cache: !config.debug,
+		root: viewPath,
+		ext: '.ect',
+		open: '{%',
+		close: '%}',
+	}).render);
+
+	app.get('/', function (req, res) {
+		res.render('index')
+	})
+}
