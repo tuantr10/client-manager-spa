bfb3477b3e86545718aef3716969e6469ba88bcf
Tuan Dung TRAN <mcafee92@gmail.com>
2017-09-10 23:26:54 +0900

    serve static files for express

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(morgan('combined'));
 
+app.use(express.static('public'))
+
 desktopApp.init(app);
 apiApp.init(app);
 
