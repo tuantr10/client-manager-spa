2a3a85697da65dc14341aee054a63e3236e3a051
Tuan Dung TRAN <mcafee92@gmail.com>
2017-09-17 01:03:55 +0900

    restyle layout

--- public/src/components/employees.js
+++ public/src/components/employees.js
@@ -7,24 +7,29 @@ export default class Employees extends Component {
 	render() {
 		const { employees } = this.props;
 		return (
-			<div className="table-responsive">			
-				<table className='table table-striped'>
-					<thead>
-						<tr className="info">
-							<th>ID</th>
-							<th>Name</th>
-							<th>Email</th>
-							<th>Address</th>
-							<th>Phone</th>
-							<th>Salary</th>
-						</tr>
-					</thead>
-					<tbody>
-						{employees.map(employee => (
-							<Employee key={ employee.id } employee={ employee } />
-						))}
-					</tbody>
-				</table>
+			<div>
+				<h2 className="text-center">Mothership | Employee Manager</h2>
+				<button className="btn btn-sm btn-primary">Add New Employee</button>
+				<div className="table-responsive">
+					<table className='table table-striped'>
+						<thead>
+							<tr className="info">
+								<th>#</th>
+								<th>Name</th>
+								<th>Email</th>
+								<th>Address</th>
+								<th>Phone</th>
+								<th>Salary</th>
+								<th colSpan={ 2 } className="text-center">Actions</th>
+							</tr>
+						</thead>
+						<tbody>
+							{employees.map(employee => (
+								<Employee key={ employee.id } employee={ employee } />
+							))}
+						</tbody>
+					</table>
+				</div>
 			</div>
 		);
 	}
