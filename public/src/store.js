1e005bacd1701a23b23f017d8ada83bfea8fd3f4
Tuan Dung TRAN <mcafee92@gmail.com>
2017-09-22 15:42:09 +0900

    temporary remove logger

--- public/src/store.js
+++ public/src/store.js
@@ -1,9 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
 import reducer from './reducers';
-import { createLogger } from 'redux-logger';
+//import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
 
-const middleware = applyMiddleware(promise(), thunk, createLogger());
+const middleware = applyMiddleware(promise(), thunk);
 
 export default createStore(reducer, middleware);
